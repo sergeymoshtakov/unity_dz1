674d0407579d648f5091e8063e98fe7ac06a95b6
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-11-13 13:09:27 +0100

    first commit

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    //public void Start()
+    //{
+    //    print($"Player speed = {_speed}");
+    //}
+
+    public void Update()
+    {
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    print($"Player speed = {_speed}");
+        //}
+
+        var verticalInput = Input.GetAxis("Vertical");
+        var movementVector = new Vector3(0f, 0f, verticalInput * _speed * Time.deltaTime);
+
+        transform.Translate(movementVector);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# unity_dz1
+
+![result](images/result-1.png)
